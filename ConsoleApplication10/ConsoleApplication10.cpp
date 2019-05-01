1b105d66172722c276e6a1d6effdc3a92d48caa7
Piotr Dębowski <piotr.debowski01@student.wat.edu.pl>
2019-05-02 01:20:16 +0200

    delete do dokończenia

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -4,8 +4,47 @@
 #include "pch.h"
 #include <iostream>
 
-using namespace std;
+struct node
+{
+	int data;
+	struct node * next;
+};
+struct node * head = NULL;
+struct node * tail = NULL;
+
+void insert(int new_data);
+void display(node * ptr);
+void delete_data(int n);
+
 int main()
 {
-    
+	insert(1);
+	insert(4);
+	display(head);
+	return 0;
+}
+
+void insert(int new_data)
+{
+	struct node * new_node = new node;
+	new_node->data = new_data;
+	new_node->next = head;
+	head = new_node;
+}
+
+void display(node * ptr)
+{
+	while (ptr != NULL)
+	{
+		std::cout << ptr->data << " ";
+		ptr = ptr->next;
+	}
+}
+
+void delete_data(int n)
+{
+	for (int i = 0; i < n-1; i++)
+	{
+
+	}
 }
